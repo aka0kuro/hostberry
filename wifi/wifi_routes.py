19b95cf7f143e902a89e91275860e6f3cb4fd0aa
aka0kuro <aka.kuro@disroot.org>
2025-05-10 01:57:36 +0200

    Auto-commit: cambios del 2025-05-10 01:57:36

--- wifi/wifi_routes.py
+++ wifi/wifi_routes.py
@@ -7,7 +7,10 @@ from .wifi_utils import wifi_utils
 
 wifi_bp = Blueprint('wifi', __name__)
 
+from flask_wtf.csrf import csrf_exempt
+
 @wifi_bp.route('/api/wifi/connect', methods=['GET', 'POST'])
+@csrf_exempt
 def wifi_connect():
     """Conecta a una red WiFi y SIEMPRE genera el archivo wpa_supplicant.conf aunque la conexión falle."""
     try:
