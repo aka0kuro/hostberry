03031a95700eb3f671e5ad75b80bc50b9a128207
aka0kuro <carlos@hostberry.local>
2025-06-14 18:41:27 +0200

    Auto-commit: Cambios automáticos 2025-06-14 18:41:27

---
+++ app/services/__init__.py
@@ -0,0 +1,5 @@
+def init_services(app):
+    """Inicializa los servicios de la aplicación"""
+    # Esta función se puede usar para inicializar servicios que necesiten
+    # acceso al contexto de la aplicación
+    pass
