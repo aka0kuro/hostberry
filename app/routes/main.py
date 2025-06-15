ce510661ac002e94bde7d4653227cfbe237dd536
aka0kuro <aka.kuro@disroot.org>
2025-06-15 23:07:09 +0200

    Auto-commit: cambios del 2025-06-15 23:07:09

--- app/routes/main.py
+++ app/routes/main.py
@@ -1,5 +1,5 @@
-from flask import Blueprint, render_template, jsonify
-from app.utils.i18n_utils import get_locale, inject_get_locale, set_language, check_lang
+from flask import Blueprint, render_template, jsonify, redirect, request, url_for
+from app.utils.i18n_utils import get_locale, inject_get_locale, set_language as set_language_util, check_lang
 from app.utils.log_utils import get_logs
 from app.utils.security_utils import FAILED_ATTEMPTS, BLOCKED_IPS
 from flask_login import login_required
@@ -20,3 +20,9 @@ def status():
         'version': '1.0.0',
         'environment': 'development'
     })
+
+@main_bp.route('/set_language/<lang>')
+def set_language(lang):
+    """Establece el idioma de la sesión."""
+    set_language_util(lang)
+    return redirect(request.referrer or url_for('main.index'))
