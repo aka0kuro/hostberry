40149e67d2414ab013f84fd136b7bebc57dde916
aka0kuro <aka.kuro@disroot.org>
2025-07-07 19:14:37 +0200

    Auto-commit: cambios del 2025-07-07 19:14:37

--- app/routes/main.py
+++ app/routes/main.py
@@ -1,4 +1,5 @@
-from flask import Blueprint, render_template, jsonify, redirect, request, url_for
+from flask import Blueprint, render_template, jsonify, redirect, request, url_for, current_app
+from flask_babel import _
 from app.utils.i18n_utils import get_locale, inject_get_locale, set_language as set_language_util, check_lang
 from app.utils.log_utils import get_logs
 from app.utils.security_utils import FAILED_ATTEMPTS, BLOCKED_IPS
@@ -6,11 +7,19 @@ from flask_login import login_required
 
 main_bp = Blueprint('main', __name__)
 
+# Registrar la función _ en el contexto de Jinja2
+@main_bp.app_context_processor
+def inject_babel():
+    from flask_babel import gettext as _
+    return dict(_=_)
+
 @main_bp.route('/')
 @login_required
 def index():
     """Página principal de la aplicación"""
-    return render_template('index.html')
+    # Ejemplo de uso de traducción
+    welcome_message = _('Welcome to HostBerry')
+    return render_template('index.html', welcome_message=welcome_message)
 
 @main_bp.route('/api/status')
 def status():
