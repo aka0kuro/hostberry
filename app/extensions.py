de3df2b316ce2fa49c1f117c9c088ac6cf1e9f76
aka0kuro <aka.kuro@disroot.org>
2025-07-11 19:20:56 +0200

    Auto-commit: cambios del 2025-07-11 19:20:56

--- app/extensions.py
+++ app/extensions.py
@@ -6,12 +6,15 @@ from flask_login import LoginManager
 from flask_wtf.csrf import CSRFProtect
 from flask_babel import Babel
 from flask_migrate import Migrate
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
 
 # Inicializar extensiones
 db = SQLAlchemy()
 login_manager = LoginManager()
 csrf = CSRFProtect()
 migrate = Migrate()
+limiter = Limiter(key_func=get_remote_address)
 
 # Configuración de Babel
 babel = Babel()
@@ -21,3 +24,8 @@ login_manager.login_view = 'auth.login'
 login_manager.login_message_category = 'warning'
 login_manager.login_message = 'Por favor inicia sesión para acceder a esta página.'
 login_manager.session_protection = 'strong'
+
+# Ejemplo de uso de limiter en una ruta:
+# from app.extensions import limiter
+# @limiter.limit("10 per minute")
+# def my_route(): ...
