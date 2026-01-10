4a88a1bdaa990155fd5e23cf8357871456193fb6
aka0kuro <aka.kuro@disroot.org>
2026-01-10 12:29:16 +0100

    Auto-commit: cambios detectados 2026-01-10 12:29:16

--- website/static/js/network.js
+++ website/static/js/network.js
@@ -635,7 +635,7 @@
         packetsSent = sentPackets;
         errors = recvErrs + sentErrs;
         drop = recvDrop + sentDrop;
-        console.log(`Found interface ${interfaceName}: recv=${recvBytes}, sent=${sentBytes}`);
+        console.log(`✓ Found interface ${interfaceName}: recv=${recvBytes}, sent=${sentBytes}, packets_recv=${recvPackets}, packets_sent=${sentPackets}`);
         break; // Salir del loop cuando se encuentra la interfaz especificada
       } else if (!interfaceName) {
         // Si no se especificó interfaz, usar la primera no-loopback con tráfico
