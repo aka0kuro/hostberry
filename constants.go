2e577207b77d4c513d5a70d3ce5b333f0e7ece5f
aka0kuro <aka.kuro@disroot.org>
2026-01-10 15:27:31 +0100

    Auto-commit: cambios detectados 2026-01-10 15:27:31

---
+++ constants.go
@@ -0,0 +1,19 @@
+package main
+
+// Constantes de configuración por defecto
+const (
+	// DefaultWiFiInterface es la interfaz WiFi por defecto
+	DefaultWiFiInterface = "wlan0"
+	
+	// DefaultCountryCode es el código de país por defecto para WiFi
+	DefaultCountryCode = "US"
+	
+	// DefaultServerHost es el host por defecto del servidor
+	DefaultServerHost = "0.0.0.0"
+	
+	// DefaultServerPort es el puerto por defecto del servidor
+	DefaultServerPort = 8000
+	
+	// DefaultUnknownValue es el valor por defecto para campos desconocidos
+	DefaultUnknownValue = "N/A"
+)
